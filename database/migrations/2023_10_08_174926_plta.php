df8b1024812574ee3d954284cbb3391c9c37bee9
sukriyatma <91186346+sukriyatma@users.noreply.github.com>
2023-10-10 19:01:31 +0700

    migrate plta table

--- database/migrations/2023_10_08_174926_plta.php
+++ database/migrations/2023_10_08_174926_plta.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,15 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('pltas', function (Blueprint $table) {
+        Schema::create('plta', function (Blueprint $table) {
             $table->uuid('id')->default(DB::raw('uuid_generate_v4()'))->primary();
 
-            $table->uuid('id_pl')->unsigned()->index();
-            $table->foreign('id_pl')->references('id')->on('pembangkits')->onDelete('cascade');
+            $table->uuid('id_pl')->nullable()->unsigned()->index();
+            $table->foreign('id_pl')->references('id')->on('pembangkit')->onDelete('cascade');
 
             $table->string('tipe_pembangkit')->nullable();
             $table->string('unit_pembangkit')->nullable();
             $table->timestamps();
+        });
+
+        Schema::table('plta', function (Blueprint $table) {
+            $table->softDeletes();
         });
     }
 
@@ -29,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('pltas');
+        Schema::dropIfExists('plta');
     }
 };
