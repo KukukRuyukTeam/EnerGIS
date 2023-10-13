ddf9e2333aea04d96de76e84f64d54ba998f966d
sukriyatma <91186346+sukriyatma@users.noreply.github.com>
2023-10-13 21:54:52 +0700

    generate Model PLTBm

---
+++ app/Models/PLTBm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PLTBm extends Model
+{
+    use SoftDeletes;
+    protected $table = 'pltbm';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $timestamps = true;
+    protected array $date = ['deleted_at'];
+
+    protected $fillable = [
+        'id_pl',
+        'sumber_energi'
+    ];
+
+    public function pembangkit() {
+        return $this->hasOne(Pembangkit::class, 'id', 'id_pl');
+    }
+}
