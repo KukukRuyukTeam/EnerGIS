75a5953ae3e32503b081de8c36ba981543dc55d8
sukriyatma <91186346+sukriyatma@users.noreply.github.com>
2023-10-18 15:52:52 +0700

    add Admin function

---
+++ app/Models/Admin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model implements Authenticatable
+{
+    protected $table = 'admin';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $timestamps = true;
+
+    protected $fillable = [
+      'email',
+      'password',
+      'nama',
+      'token'
+    ];
+
+
+    public function getAuthIdentifierName()
+    {
+        return 'email';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->email;
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRememberToken()
+    {
+        return $this->token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'token';
+    }
+}
---
+++ database/migrations/2023_10_17_104441_admin.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->uuid('id')->default(DB::raw('uuid_generate_v4()'))->primary();
+            $table->string('email')->nullable(false)->unique('unique_email_index');
+            $table->string('password')->nullable(false);
+            $table->string('nama')->nullable();
+            $table->string('token')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin');
+    }
+};
